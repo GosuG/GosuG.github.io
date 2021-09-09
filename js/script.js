a11a5ebef9e7fc0fd717df1970c2a19e3b5dc8a6
GosuG <vanyigabor.94@gmail.com>
2021-09-09 22:35:18 +0200

    Init page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-link li');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation ='';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            }
+            
+        });
+
+        burger.classList.toggle('toggle');
+
+    });
+    
+}
+
+navSlide();
+
+
+
+
+
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
